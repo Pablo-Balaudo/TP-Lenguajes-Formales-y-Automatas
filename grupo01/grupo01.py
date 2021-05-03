2203f1aadf982f1f2f1521f9d18a5c0899ff6aac
JesicaHeit <51091834+JesicaHeit@users.noreply.github.com>
2021-05-03 18:33:57 -0300

    cambios

--- grupo01/grupo01.py
+++ grupo01/grupo01.py
@@ -6,13 +6,34 @@ reserved=('SELECT','FROM', 'INNER JOIN', 'LEFT JOIN', 'RIGHT JOIN', 'AS','WHERE'
 
 tokens= reserved + (
 #OPERADORES (or, and, not in, is null, is not null, <, <=, >, >=, count, min, max)
-    'or', 'and', 'not in', 'is null', 'is not null', 'LT', 'LE', 'GT', 'GE', 'count', 'min', 'max'
+    'or', 'and', 'not in', 'is null', 'is not null', 'LT', 'LE', 'GT', 'GE', 'count', 'min', 'max',
+
+#DELIMITADORES ( ) [ ] , .
+    'LPAREN', 'RPAREN',
+    'LBRACKET', 'RBRACKET',
+    'COMMA', 'PERIOD',
+
 #demás datos (nombres de tablas, campos, etc)
-    'NAME'
+    'DATOS'
 )
 
-t_NAME = r'[a-zA-Z_]*'
+t_DATOS = r'[a-zA-Z_]*' #solo vamos a hacer que los nombres tengan unicamente letras? o incluiremos alfanumerico? VER BIEN
+
 t_ignore=" \t" #ignoramos el espacio vacío (ver bien si sql ejecuta una query si le elimino los espacios vacíos)
+
+#operaores
+t_LT= r'<'
+t_GT= r'>'
+t_LE= r'<='
+t_GE= r'>='
+
+#delimitadores
+t_LPAREN= r'\('
+t_RPAREN= r'\)'
+t_LBRACKET= r'\['
+t_RBRACKET= r'\]'
+t_COMMA= r','
+t_PERIOD= r'\.'
 
 def t_error(t):
     print ("Caracter ilegal '%s" % t.value[0])
