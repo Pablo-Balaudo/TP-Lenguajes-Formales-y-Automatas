b51342c741d64e5894609822647e8fd8b739950d
Pablo-Balaudo <Balaudo.Pablo@protonmail.com>
2021-05-27 18:52:53 -0300

    Improved lex Added strings Fixed whitespace on reserved words Fixed COLUMN_NAME taking a period as part of the token

--- grupo01/grupo01.py
+++ grupo01/grupo01.py
@@ -6,19 +6,21 @@ reserved = {
     'AS': 'AS',
     'ASC': 'ASC',
     'BETWEEN': 'BETWEEN',
+    'BY': 'BY',
     'COUNT': 'COUNT',
     'DESC': 'DESC',
     'DISTINCT': 'DISTINCT',
     'FROM': 'FROM',
-    'GROUP_BY': 'GROUP_BY',
+    'GROUP': 'GROUP',
     'HAVING': 'HAVING',
-    'INNER_JOIN': 'INNER_JOIN',
-    'LEFT_JOIN': 'LEFT_JOIN',
+    'INNER': 'INNER',
+    'JOIN': 'JOIN',
+    'LEFT': 'LEFT',
     'MAX': 'MAX',
     'MIN': 'MIN',
     'ON': 'ON',
-    'ORDER_BY': 'ORDER_BY',
-    'RIGHT_JOIN': 'RIGHT_JOIN',
+    'ORDER': 'ORDER',
+    'RIGHT': 'RIGHT',
     'SELECT': 'SELECT',
     'WHERE': 'WHERE',
 
@@ -35,7 +37,7 @@ tokens = list(reserved.values()) + [
     'LPAREN', 'RPAREN', 'LBRACKET', 'RBRACKET', 'COMMA', 'PERIOD',
 
     # Other data (name of the table, name of the column, ID for reserved words lookup)
-    'TABLE_NAME', 'COLUMN_NAME', 'ID', 'NUMBER'
+    'TABLE_NAME', 'COLUMN_NAME', 'STRING', 'NUMBER'
 ]
 
 t_EQUAL_TO = r'\='
@@ -50,29 +52,21 @@ t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_LBRACKET = r'\['
 t_RBRACKET = r'\]'
-t_COMMA = r','
+t_COMMA = r'\,'
 t_PERIOD = r'\.'
 
 
-# Ignore newline
-def t_newline(t):
-    r"""\n+"""
-    t.lexer.lineno += len(t.value)
+STRING = r'\'' + r'[a-zA-Z_0-9_ ]*' + r'\''
 
 
-# Ignore whitespace
-t_ignore = ' \t'
-
-
-# Check for reserved words
-# def t_ID(t):
-#     r"""[a-zA-Z_][a-zA-Z_0-9]*"""
-#     t.type = reserved.get(t.value, 'ID')    # Check for reserved words
-#     return t
+@lex.TOKEN(STRING)
+def t_STRING(t):
+    t.type = reserved.get(t.value, 'STRING')    # Check for reserved words
+    return t
 
 
 def t_COLUMN_NAME(t):
-    r""".[a-zA-Z_][a-zA-Z_0-9]*"""
+    r"""[a-zA-Z_][a-zA-Z_0-9]*_.[a-zA-Z_][a-zA-Z_0-9]*"""
     t.type = reserved.get(t.value, 'COLUMN_NAME')    # Check for reserved words
     return t
 
@@ -89,6 +83,16 @@ def t_NUMBER(t):
     return t
 
 
+# Ignore newline
+def t_newline(t):
+    r"""\n+"""
+    t.lexer.lineno += len(t.value)
+
+
+# Ignore whitespace
+t_ignore = ' \t'
+
+
 # Error handling rule
 def t_error(t):
     print("Caracter ilegal '%s" % t.value[0])
@@ -103,17 +107,27 @@ FROM Productos
 WHERE NombreProducto BETWEEN Pasta_Italiana AND Pizza 
 ORDER_BY NombreProducto ASC""")
 example_3 = ("""SELECT fecha_operacion, fecha_presentacion_srt, tipo_operacion, tipo_operacion_modificacion AS Operacion_de, ectm.descripcion AS Tipo_de_Modificacion, ec.periodo_prima, ec.monto_fijo_alic, ec.porc_alic 
-FROM EMIS_Contratos_SRT ec INNER_JOIN EMIS_Contratos_Tipos_Modificaciones ectm ON tipo_modificacion = cod_tipo_modificacion 
+FROM EMIS_Contratos_SRT ec INNER JOIN EMIS_Contratos_Tipos_Modificaciones ectm ON tipo_modificacion = cod_tipo_modificacion 
 WHERE nro_contrato = 753603 AND ec.Fecha_Baja = 0 
 ORDER_BY ec.fecha_operacion DESC""")
 example_4 = """SELECT eci.nro_contrato, oa.zona, oa.subzona, oa.nro_org, eci.nro_prod, ia.razon_social, oa.correo_electronico, eci.fecha_abm 
-FROM EMIS_Contrato_Intermediarios AS eci INNER_JOIN Organizadores_Aux AS oa ON eci.nro_empsoc = oa.nro_empsoc AND eci.nro_org = oa.nro_org 
+FROM EMIS_Contrato_Intermediarios AS eci INNER JOIN Organizadores_Aux AS oa ON eci.nro_empsoc = oa.nro_empsoc AND eci.nro_org = oa.nro_org 
 WHERE nro_contrato = eci.nro_org =244 ORDER BY eci.fecha_abm DESC"""
 example_5 = """ SELECT nro FROM Tabla T"""
 example_6 = '''SELECT c.first_name, c.last_name
 FROM customers AS c'''
+example_7 = '''SELECT c.first_name, c.last_name
+FROM customers AS c
+WHERE c.id = 2'''
+example_8 = '''SELECT DISTINCT c.first_name, c.last_name, p.number
+FROM customers AS c
+LEFT JOIN phones_numbers AS p ON
+c.id = p.customer_id
+WHERE c.first_name = 'hola que tal 123'
+'''
 
-lexer.input(example_6)
+
+lexer.input(example_8)
 
 while True:
     tok = lexer.token()
@@ -142,56 +156,56 @@ def p_select_where_in(p):
 
 
 def p_select_where_orderby(p):
-    """query : SELECT column_names FROM tables_name WHERE conditions ORDER_BY condition"""
+    """query : SELECT column_names FROM tables_name WHERE conditions ORDER BY condition"""
     tables[p[2]]
 
 
 def p_select_where_orderby_desc(p):
-    """query : SELECT column_names FROM tables_name WHERE conditions ORDER_BY condition DESC"""
+    """query : SELECT column_names FROM tables_name WHERE conditions ORDER BY condition DESC"""
     tables[p[2]]
 
 
 def p_select_where_orderby_asc(p):
-    """query : SELECT column_names FROM tables_name WHERE conditions ORDER_BY condition ASC"""
+    """query : SELECT column_names FROM tables_name WHERE conditions ORDER BY condition ASC"""
     tables[p[2]]
 
 
 def p_select_where_groupby_orderby_(p):
-    """query : SELECT column_names FROM tables_name WHERE conditions GROUP_BY column_names ORDER_BY condition"""
+    """query : SELECT column_names FROM tables_name WHERE conditions GROUP BY column_names ORDER BY condition"""
     tables[p[2]]
 
 
 def p_select_where_groupby_having_orderby_(p):
-    """query : SELECT column_names FROM tables_name WHERE conditions GROUP_BY column_names HAVING condition ORDER_BY
+    """query : SELECT column_names FROM tables_name WHERE conditions GROUP BY column_names HAVING condition ORDER BY
     condition """
     tables[p[2]]
 
 
 def p_select_innerjoin_where(p):
-    """query : SELECT column_names FROM tables_name INNER_JOIN tables_name WHERE conditions"""
+    """query : SELECT column_names FROM tables_name INNER JOIN tables_name WHERE conditions"""
     tables[p[2]]
 
 
 def p_select_innerjoin_on_where(p):
-    """query : SELECT column_names FROM tables_name INNER_JOIN tables_name ON conditions WHERE conditions"""
+    """query : SELECT column_names FROM tables_name INNER JOIN tables_name ON conditions WHERE conditions"""
     tables[p[2]]
 
 
 def p_select_leftjoin_where(p):
-    """query : SELECT column_names FROM tables_name LEFT_JOIN tables_name WHERE conditions"""
+    """query : SELECT column_names FROM tables_name LEFT JOIN tables_name WHERE conditions"""
     tables[p[2]]
 
 
 def p_select_leftjoin_on_where(p):
-    """query : SELECT column_names FROM tables_name LEFT_JOIN tables_name ON conditions WHERE conditions"""
+    """query : SELECT column_names FROM tables_name LEFT JOIN tables_name ON conditions WHERE conditions"""
     tables[p[2]]
 
 
 def p_select_rightjoin_where(p):
-    """query : SELECT column_names FROM tables_name RIGHT_JOIN tables_name WHERE conditions"""
+    """query : SELECT column_names FROM tables_name RIGHT JOIN tables_name WHERE conditions"""
     tables[p[2]]
 
 
 def p_select_rightjoin_on_where(p):
-    """query : SELECT column_names FROM tables_name RIGHT_JOIN tables_name ON conditions WHERE conditions"""
+    """query : SELECT column_names FROM tables_name RIGHT JOIN tables_name ON conditions WHERE conditions"""
     tables[p[2]]
