ae2caaa36eaae81c06ade52ff3890f84ef37aabf
Pablo-Balaudo <Balaudo.Pablo@protonmail.com>
2021-06-02 18:30:58 -0300

    Final touches to lex Added prototype grammar Added empty/lambda for it's use in grammar Changed the implementation of regular expression to use decorators instead of docstrings Removed unused reserved words Removed unusable examples Code cleanup

--- grupo01/grupo01.py
+++ grupo01/grupo01.py
@@ -5,7 +5,6 @@ import ply.yacc as yacc
 reserved = {
     'AS': 'AS',
     'ASC': 'ASC',
-    'BETWEEN': 'BETWEEN',
     'BY': 'BY',
     'COUNT': 'COUNT',
     'DESC': 'DESC',
@@ -20,11 +19,8 @@ reserved = {
     'MIN': 'MIN',
     'ON': 'ON',
     'ORDER': 'ORDER',
-    'RIGHT': 'RIGHT',
     'SELECT': 'SELECT',
     'WHERE': 'WHERE',
-
-
 
     # Logical Operators (AND, OR)
     'AND': 'AND',
@@ -35,13 +31,16 @@ tokens = list(reserved.values()) + [
     # Comparison Operators (=, >, <, >=, <=, <>)
     'EQUAL_TO', 'GREATER_THAN', 'LESS_THAN', 'GREATER_THAN_OR_EQUAL_TO', 'LESS_THAN_OR_EQUAL_TO', 'NOT_EQUAL_TO',
 
-    # DELIMITADORES (( ) [ ] , .)
-    'LPAREN', 'RPAREN', 'LBRACKET', 'RBRACKET', 'COMMA', 'PERIOD',
+    # Delimiters (( ) [ ] , .)
+    'LEFT_PARENTHESIS', 'RIGHT_PARENTHESIS', 'LEFT_BRACKET', 'RIGHT_BRACKET', 'COMMA', 'PERIOD',
 
     # Other data (name of the table, name of the column, ID for reserved words lookup)
-     'TABLE_NAME','COLUMN_NAME', 'STRING', 'NUMBER'
+    'TABLE_NAME', 'COLUMN_NAME', 'STRING', 'NUMBER'
 ]
 
+# Definitions of Regular Expressions
+
+# Operators
 t_EQUAL_TO = r'\='
 t_GREATER_THAN = r'\>'
 t_LESS_THAN = r'\<'
@@ -49,45 +48,51 @@ t_GREATER_THAN_OR_EQUAL_TO = r'\>='
 t_LESS_THAN_OR_EQUAL_TO = r'\<='
 t_NOT_EQUAL_TO = r'\<>'
 
-# delimitadores
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_LBRACKET = r'\['
-t_RBRACKET = r'\]'
+# Delimiters
+t_LEFT_PARENTHESIS = r'\('
+t_RIGHT_PARENTHESIS = r'\)'
+t_LEFT_BRACKET = r'\['
+t_RIGHT_BRACKET = r'\]'
 t_COMMA = r'\,'
 t_PERIOD = r'\.'
 
+# Simple REs
+RE_Number = r'\d+'
+RE_Newline = r'\n+'
 
-STRING = r'\'' + r'[a-zA-Z_0-9_ ]*' + r'\''
+# Complex REs
+RE_String = r'\'[a-zA-Z_0-9_ ]*\''
+RE_Table = r'[a-zA-Z][\_a-zA-Z0-9]*'
+RE_Column = RE_Table + t_PERIOD + RE_Table
 
 
-@lex.TOKEN(STRING)
-
+@lex.TOKEN(RE_String)
 def t_STRING(t):
-    t.type = reserved.get(t.value, 'STRING')    # Check for reserved words
+    t.type = reserved.get(t.value, 'STRING')  # Check for reserved words
     return t
 
 
+@lex.TOKEN(RE_Column)
 def t_COLUMN_NAME(t):
-    r"""[a-zA-Z][\_a-zA-Z0-9]*\.[a-zA-Z][\_a-zA-Z0-9]*"""
-    t.type = reserved.get(t.value, 'COLUMN_NAME')    # Check for reserved words
+    t.type = reserved.get(t.value, 'COLUMN_NAME')  # Check for reserved words
     return t
 
+
+@lex.TOKEN(RE_Table)
 def t_TABLE_NAME(t):
-    r"""[a-zA-Z][\_a-zA-Z0-9]*"""
-    t.type = reserved.get(t.value, 'TABLE_NAME')    # Check for reserved words
+    t.type = reserved.get(t.value, 'TABLE_NAME')  # Check for reserved words
     return t
 
 
+@lex.TOKEN(RE_Number)
 def t_NUMBER(t):
-    r"""\d+"""
     t.value = int(t.value)
     return t
 
 
 # Ignore newline
+@lex.TOKEN(RE_Newline)
 def t_newline(t):
-    r"""\n+"""
     t.lexer.lineno += len(t.value)
 
 
@@ -103,41 +108,35 @@ def t_error(t):
 
 lexer = lex.lex()
 
-example_1 = """SELECT COUNT (DISTINCT NombreProducto) FROM Productos"""
-example_2 = ("""SELECT NombreProducto 
-FROM Productos 
-WHERE NombreProducto BETWEEN Pasta_Italiana AND Pizza 
-ORDER BY NombreProducto ASC""")
-example_3 = ("""SELECT fecha_operacion, fecha_presentacion_srt, tipo_operacion, tipo_operacion_modificacion AS Operacion_de, ectm.descripcion AS Tipo_de_Modificacion, ec.periodo_prima, ec.monto_fijo_alic, ec.porc_alic 
-FROM EMIS_Contratos_SRT AS ec INNER JOIN EMIS_Contratos_Tipos_Modificaciones AS ectm ON tipo_modificacion = cod_tipo_modificacion 
-WHERE nro_contrato = 753603 AND ec.Fecha_Baja = 0 
-ORDER BY ec.fecha_operacion DESC""")
-example_4 = """SELECT eci.nro_contrato, oa.zona, oa.subzona, oa.nro_org, 
+example_1 = """SELECT eci.nro_contrato, oa.zona, oa.subzona, oa.nro_org, 
 eci.nro_prod, ia.razon_social, oa.correo_electronico, eci.fecha_abm 
 FROM EMIS_Contrato_Intermediarios AS eci INNER JOIN Organizadores_Aux AS oa ON 
 eci.nro_empsoc = oa.nro_empsoc AND eci.nro_org = oa.nro_org 
 WHERE eci.nro_org =244 ORDER BY eci.fecha_abm DESC"""
-example_5 = """ SELECT nro FROM Tabla AS T"""
-example_6 = '''SELECT c.first_name, c.last_name
+
+example_2 = """ SELECT nro FROM Tabla AS T"""
+
+example_3 = '''SELECT c.first_name, c.last_name
 FROM customers AS c'''
-example_7 = '''SELECT c.first_name, c.last_name
+
+example_4 = '''SELECT c.first_name, c.last_name
 FROM customers AS c
 WHERE c.id = 2'''
-example_8 = '''SELECT DISTINCT c.first_name, c.last_name, p.number
+
+example_5 = '''SELECT DISTINCT c.first_name, c.last_name, p.number
 FROM customers AS c
 LEFT JOIN phones_numbers AS p ON
 c.id = p.customer_id
-WHERE c.first_name = 'hola que tal 123'
+WHERE [c.first_name = 'hola que tal 123']
+AND (p.number = 123)
 '''
 
-
-
-lexer.input(example_7)
+lexer.input(example_5)
 
 while True:
     tok = lexer.token()
     if not tok:
-        break          # No more input
+        break  # No more input
     print(tok)
 
 # dictionary of tables
@@ -214,3 +213,53 @@ def p_select_rightjoin_where(p):
 def p_select_rightjoin_on_where(p):
     """query : SELECT column_names FROM tables_name RIGHT JOIN tables_name ON conditions WHERE conditions"""
     tables[p[2]]
+
+
+def p_empty(p):
+    """empty :"""
+    pass
+
+
+# Grammar
+"""
+Axiom : SELECT NT_Columns FROM NT_Tables NT_WHERE
+
+NT_Columns : COLUMN_NAME NT_Auxiliary_Column 
+
+NT_Auxiliary_Column : empty
+                    | t_PERIOD COLUMN_NAME NT_Auxiliary_Column
+
+NT_Tables : TABLE_NAME NT_Auxiliary_Table
+
+NT_Auxiliary_Table : empty
+                   | t_PERIOD TABLE_NAME NT_Auxiliary_Table
+                   | AS TABLE_NAME NT_FIX_MULTIPLE_CONSECUTIVE_AS_PROBLEM
+
+NT_FIX_MULTIPLE_CONSECUTIVE_AS_PROBLEM : empty
+                                       | t_PERIOD TABLE_NAME NT_Auxiliary_Table
+
+NT_WHERE : empty
+         | ORDER_BY T_Condition_ORDER_BY
+         | T_Condition_WHERE NT_Auxiliary_WHERE
+
+NT_Auxiliary_WHERE : empty
+                   | ORDER_BY T_Condition_ORDER_BY
+                   | GROUP_BY T_Condition_GROUP_BY NT_Auxiliary_GROUP_BY
+
+NT_Auxiliary_GROUP_BY : empty
+                      | ORDER_BY T_Condition_ORDER_BY
+                      | Having T_Condition_Having NT_Auxiliary_Having
+
+NT_Auxiliary_Having : empty
+                    | ORDER_BY T_Condition_ORDER_BY
+
+ORDER_BY : ORDER BY
+
+GROUP_BY : GROUP BY
+
+INNER_JOIN : INNER JOIN
+
+LEFT_JOIN : LEFT JOIN
+"""
+# Note 1: NT means "No Terminal", T means "Terminal", empty means "Lambda"
+# Note 2: No need for NT_SELECT and NT_FROM because they must exist no matter what for the SQL query to work
