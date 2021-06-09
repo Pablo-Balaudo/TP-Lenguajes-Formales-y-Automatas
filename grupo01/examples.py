0ee7110bfb586981f625ef9bfe7309eba058d000
ebitschin <ebitschin@folderit.net>
2021-06-09 04:38:26 -0300

    Add initial grammar, yacc functions and errors, update .gitignore for ply gen files

---
+++ grupo01/examples.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import importlib
+
+files = os.listdir('.')
+module_file = [f.split('.')[0] for f in files if 'grupo' in f][0]
+grupo = importlib.import_module(module_file)
+
+examples = ("""SELECT eci.nro_contrato, oa.zona, oa.subzona, oa.nro_org, 
+eci.nro_prod, ia.razon_social, oa.correo_electronico, eci.fecha_abm 
+FROM EMIS_Contrato_Intermediarios AS eci INNER JOIN Organizadores_Aux AS oa ON 
+eci.nro_empsoc = oa.nro_empsoc AND eci.nro_org = oa.nro_org 
+WHERE eci.nro_org =244 ORDER BY eci.fecha_abm DESC""",
+
+""" SELECT T.asd FROM Tabla AS T""",
+
+'''SELECT c.first_name, c.last_name
+FROM customers AS c''',
+
+'''SELECT c.first_name, c.last_name
+FROM customers AS c
+WHERE c.id = 2''',
+
+'''SELECT DISTINCT c.first_name, c.last_name, p.number
+FROM customers AS c
+LEFT JOIN phones_numbers AS p ON
+c.id = p.customer_id
+WHERE c.first_name = 'hola que tal 123'
+AND (p.number = 123)
+''')
+
+for num, example in enumerate(examples):
+    print(f'-- RUNNING EXAMPLE { num + 1 } --')
+    result = grupo.parse_select_statement(example, debug = True)
+    print('\n Result: ', result, '\n')
--- grupo01/grupo01.py
+++ grupo01/grupo01.py
@@ -2,6 +2,26 @@ import sys
 import ply.lex as lex
 import ply.yacc as yacc
 
+class SelectState:
+    def __init__(self, input = None):
+        self.parsing = input
+        self.parsed_tables = dict()
+
+    def add_table(self, table):
+        parsed = self.parsed_tables.get(table)
+
+        if not parsed:
+            self.parsed_tables[table] = []
+
+    def add_column(self, table, column):
+        self.add_table(table)
+        table_columns = self.parsed_tables[table]
+        if column not in table_columns:
+            table_columns.append(column)
+    
+    def get_report(self):
+        return self.parsed_tables
+
 reserved = {
     'AS': 'AS',
     'ASC': 'ASC',
@@ -18,6 +38,7 @@ reserved = {
     'MAX': 'MAX',
     'MIN': 'MIN',
     'ON': 'ON',
+    'IN': 'IN',
     'ORDER': 'ORDER',
     'SELECT': 'SELECT',
     'WHERE': 'WHERE',
@@ -31,8 +52,8 @@ tokens = list(reserved.values()) + [
     # Comparison Operators (=, >, <, >=, <=, <>)
     'EQUAL_TO', 'GREATER_THAN', 'LESS_THAN', 'GREATER_THAN_OR_EQUAL_TO', 'LESS_THAN_OR_EQUAL_TO', 'NOT_EQUAL_TO',
 
-    # Delimiters (( ) [ ] , .)
-    'LEFT_PARENTHESIS', 'RIGHT_PARENTHESIS', 'LEFT_BRACKET', 'RIGHT_BRACKET', 'COMMA', 'PERIOD',
+    # Delimiters (( ) , .)
+    'LEFT_PARENTHESIS', 'RIGHT_PARENTHESIS', 'COMMA', 'PERIOD',
 
     # Other data (name of the table, name of the column, ID for reserved words lookup)
     'TABLE_NAME', 'COLUMN_NAME', 'STRING', 'NUMBER'
@@ -51,20 +72,18 @@ t_NOT_EQUAL_TO = r'\<>'
 # Delimiters
 t_LEFT_PARENTHESIS = r'\('
 t_RIGHT_PARENTHESIS = r'\)'
-t_LEFT_BRACKET = r'\['
-t_RIGHT_BRACKET = r'\]'
 t_COMMA = r'\,'
 t_PERIOD = r'\.'
 
 # Simple REs
 RE_Number = r'\d+'
 RE_Newline = r'\n+'
+NEW_LINE_CHAR = '\n'
 
 # Complex REs
 RE_String = r'\'[a-zA-Z_0-9_ ]*\''
 RE_Table = r'[a-zA-Z][\_a-zA-Z0-9]*'
 RE_Column = RE_Table + t_PERIOD + RE_Table
-
 
 @lex.TOKEN(RE_String)
 def t_STRING(t):
@@ -99,167 +118,225 @@ def t_newline(t):
 # Ignore whitespace
 t_ignore = ' \t'
 
+# Get column for parsed token
+def get_column(parsing, pos = 0):
+    start = parsing.rfind(NEW_LINE_CHAR, 0, pos) + 1
+
+    if start < 0:
+        start = 0
+
+    offset = pos - start
+
+    return 1 if offset == 0 else offset
+
+def get_info(token):
+    val = token.value
+    line = token.lineno
+    column = get_column(token.lexer.select_state.parsing, token.lexpos)
+    return val, line, column
 
 # Error handling rule
-def t_error(t):
-    print("Caracter ilegal '%s" % t.value[0])
-    t.lexer.skip(1)
+def t_error(token):
+    if not token:
+        raise EOFError("Unexpected EOF")
+    
+    char, line, column = get_info(token)
 
+    raise Exception(f"Ilegal character '{char[0]}' at ({line}, {column})")
 
-lexer = lex.lex()
+# YACC & GRAMMAR
+# Note 1: NT means "No Terminal", empty means "Lambda"
+# Note 2: No need for NT_SELECT and NT_FROM because they must exist no matter what for the SQL query to work
 
-example_1 = """SELECT eci.nro_contrato, oa.zona, oa.subzona, oa.nro_org, 
-eci.nro_prod, ia.razon_social, oa.correo_electronico, eci.fecha_abm 
-FROM EMIS_Contrato_Intermediarios AS eci INNER JOIN Organizadores_Aux AS oa ON 
-eci.nro_empsoc = oa.nro_empsoc AND eci.nro_org = oa.nro_org 
-WHERE eci.nro_org =244 ORDER BY eci.fecha_abm DESC"""
+# Error handling rule
+def p_error(p):
+    if not p:
+        raise EOFError("Unexpected EOF")
+    
+    val, line, column = get_info(p)
 
-example_2 = """ SELECT nro FROM Tabla AS T"""
+    raise SyntaxError(f"Sintax Error '{ val }' at ({line}, {column})")
 
-example_3 = '''SELECT c.first_name, c.last_name
-FROM customers AS c'''
+def parse_tuple(p, reduce = False):
+    result = tuple(p[1:])
 
-example_4 = '''SELECT c.first_name, c.last_name
-FROM customers AS c
-WHERE c.id = 2'''
+    if len(result) == 1:
+        value = result[0]
 
-example_5 = '''SELECT DISTINCT c.first_name, c.last_name, p.number
-FROM customers AS c
-LEFT JOIN phones_numbers AS p ON
-c.id = p.customer_id
-WHERE [c.first_name = 'hola que tal 123']
-AND (p.number = 123)
-'''
+        if value is None:
+            return value
+        
+        if reduce and type(value) is tuple:
+            return value
 
-lexer.input(example_5)
+    return result
 
-while True:
-    tok = lexer.token()
-    if not tok:
-        break  # No more input
-    print(tok)
+def p_axiom_select(p):
+    '''
+    Axiom : SELECT NT_AUX_SELECT NT_Columns FROM NT_Tables NT_SELECT_OPTIONAL
+    '''
+    p[0] = parse_tuple(p)
 
-# dictionary of tables
-tables = {}
+def p_nt_aux_select(p):
+    '''
+    NT_AUX_SELECT : empty
+                  | DISTINCT
+    '''
+    p[0] = parse_tuple(p)
 
+def p_nt_select_optional(p):
+    '''
+    NT_SELECT_OPTIONAL : empty
+                       | NT_WHERE
+                       | NT_WHERE ORDER_BY
+                       | NT_WHERE GROUP_BY
+                       | ORDER_BY
+                       | ORDER_BY GROUP_BY
+                       | GROUP_BY
+                       | NT_WHERE ORDER_BY GROUP_BY
+    '''
+    p[0] = parse_tuple(p)
 
-def p_select(p):
-    """query : SELECT column_names FROM tables_name"""
-    tables[p[2]]  # """nombre de la tabla - VER BIEN"""
+def p_nt_column(p):
+    '''
+    NT_COLUMN : TABLE_NAME PERIOD COLUMN_NAME
+              | COLUMN_NAME
+              | STRING
+              | NUMBER
+    '''
+    p[0] = parse_tuple(p)
 
+def p_nt_columns(p):
+    '''
+    NT_Columns : NT_COLUMN
+               | NT_FUNCTION LEFT_PARENTHESIS NT_COLUMN RIGHT_PARENTHESIS
+               | NT_Columns COMMA NT_Columns
+    '''
+    p[0] = parse_tuple(p, reduce = True)
 
-def p_select_where(p):
-    """query : SELECT column_names FROM tables_name WHERE conditions"""
-    tables[p[2]]
+def p_nt_function(p):
+    '''
+    NT_FUNCTION : COUNT
+                | MAX
+                | MIN
+    '''
+    p[0] = parse_tuple(p)
 
+def p_nt_tables(p):
+    '''
+    NT_Tables : TABLE_NAME NT_Auxiliary_Table
+              | TABLE_NAME NT_Auxiliary_Table NT_JOINS
+              | TABLE_NAME NT_Auxiliary_Table COMMA NT_Tables
+    '''
+    p[0] = parse_tuple(p)
 
-def p_select_where_in(p):
-    """query : SELECT column_names FROM tables_name WHERE conditions IN (SELECT column_names FROM tables_name WHERE
-    condition) """
-    tables[p[2]]
+def p_nt_auxiliary_table(p):
+    '''
+    NT_Auxiliary_Table : empty
+                       | AS TABLE_NAME
+                       | TABLE_NAME
+    '''
+    p[0] = parse_tuple(p)
 
+def p_nt_where(p):
+    '''
+    NT_WHERE : WHERE NT_Conditions
+    '''
+    p[0] = parse_tuple(p)
 
-def p_select_where_orderby(p):
-    """query : SELECT column_names FROM tables_name WHERE conditions ORDER BY condition"""
-    tables[p[2]]
+def p_nt_condition(p):
+    '''
+    NT_Condition : NT_COLUMN EQUAL_TO NT_COLUMN
+                 | NT_COLUMN GREATER_THAN NT_COLUMN
+                 | NT_COLUMN LESS_THAN NT_COLUMN
+                 | NT_COLUMN GREATER_THAN_OR_EQUAL_TO NT_COLUMN
+                 | NT_COLUMN LESS_THAN_OR_EQUAL_TO NT_COLUMN
+                 | NT_COLUMN NOT_EQUAL_TO NT_COLUMN
+                 | NT_COLUMN AND NT_COLUMN
+                 | NT_COLUMN OR NT_COLUMN
+                 | NT_COLUMN IN LEFT_PARENTHESIS Axiom RIGHT_PARENTHESIS
+    '''
+    p[0] = parse_tuple(p)
 
+def p_nt_conditions(p):
+    '''
+    NT_Conditions : NT_Condition NT_AUX_Conditions
+                  | LEFT_PARENTHESIS NT_Condition RIGHT_PARENTHESIS NT_AUX_Conditions
+                  | LEFT_PARENTHESIS NT_Condition NT_AUX_Conditions RIGHT_PARENTHESIS
+    '''
+    p[0] = parse_tuple(p)
 
-def p_select_where_orderby_desc(p):
-    """query : SELECT column_names FROM tables_name WHERE conditions ORDER BY condition DESC"""
-    tables[p[2]]
+def p_nt_aux_conditions(p):
+    '''
+    NT_AUX_Conditions : empty
+                      | AND NT_Conditions
+                      | OR NT_Conditions
+    '''
+    p[0] = parse_tuple(p)
 
+def p_order_by(p):
+    '''
+    ORDER_BY : ORDER BY NT_AUX_ORDER_BY
+    '''
+    p[0] = parse_tuple(p)
 
-def p_select_where_orderby_asc(p):
-    """query : SELECT column_names FROM tables_name WHERE conditions ORDER BY condition ASC"""
-    tables[p[2]]
+def p_nt_aux_order_by(p):
+    '''
+    NT_AUX_ORDER_BY : NT_COLUMN
+                    | NT_COLUMN ASC
+                    | NT_COLUMN DESC
+                    | NT_COLUMN COMMA NT_AUX_ORDER_BY
+    '''
+    p[0] = parse_tuple(p)
 
+def p_group_by(p):
+    '''
+    GROUP_BY : GROUP BY NT_Columns AUX_HAVING
+    '''
+    p[0] = parse_tuple(p)
 
-def p_select_where_groupby_orderby_(p):
-    """query : SELECT column_names FROM tables_name WHERE conditions GROUP BY column_names ORDER BY condition"""
-    tables[p[2]]
+def p_aux_having(p):
+    '''
+    AUX_HAVING : empty
+               | HAVING NT_Conditions
+    '''
+    p[0] = parse_tuple(p)
 
+def p_nt_join(p):
+    '''
+    NT_JOIN : JOIN TABLE_NAME NT_Auxiliary_Table ON NT_Conditions
+    '''
+    p[0] = parse_tuple(p)
 
-def p_select_where_groupby_having_orderby_(p):
-    """query : SELECT column_names FROM tables_name WHERE conditions GROUP BY column_names HAVING condition ORDER BY
-    condition """
-    tables[p[2]]
-
-
-def p_select_innerjoin_where(p):
-    """query : SELECT column_names FROM tables_name INNER JOIN tables_name WHERE conditions"""
-    tables[p[2]]
-
-
-def p_select_innerjoin_on_where(p):
-    """query : SELECT column_names FROM tables_name INNER JOIN tables_name ON conditions WHERE conditions"""
-    tables[p[2]]
-
-
-def p_select_leftjoin_where(p):
-    """query : SELECT column_names FROM tables_name LEFT JOIN tables_name WHERE conditions"""
-    tables[p[2]]
-
-
-def p_select_leftjoin_on_where(p):
-    """query : SELECT column_names FROM tables_name LEFT JOIN tables_name ON conditions WHERE conditions"""
-    tables[p[2]]
-
-
-def p_select_rightjoin_where(p):
-    """query : SELECT column_names FROM tables_name RIGHT JOIN tables_name WHERE conditions"""
-    tables[p[2]]
-
-
-def p_select_rightjoin_on_where(p):
-    """query : SELECT column_names FROM tables_name RIGHT JOIN tables_name ON conditions WHERE conditions"""
-    tables[p[2]]
-
+def p_nt_joins(p):
+    '''
+    NT_JOINS : empty
+             | NT_JOIN NT_JOINS
+             | INNER NT_JOIN NT_JOINS
+             | LEFT NT_JOIN NT_JOINS
+    '''
+    p[0] = parse_tuple(p)
 
 def p_empty(p):
     """empty :"""
     pass
 
+lexer  = lex.lex()
+parser = yacc.yacc()
 
-# Grammar
-"""
-Axiom : SELECT NT_Columns FROM NT_Tables NT_WHERE
+def build_select_report(parsed_result):
+    pass
 
-NT_Columns : COLUMN_NAME NT_Auxiliary_Column 
+def parse_select_statement(select_input, debug = False):
+    lexer.lineno = 1
 
-NT_Auxiliary_Column : empty
-                    | t_PERIOD COLUMN_NAME NT_Auxiliary_Column
+    parser_state = SelectState(select_input)
+    lexer.select_state  = parser_state
+    parser.select_state = parser_state
 
-NT_Tables : TABLE_NAME NT_Auxiliary_Table
+    parsed = parser.parse(select_input)
 
-NT_Auxiliary_Table : empty
-                   | t_PERIOD TABLE_NAME NT_Auxiliary_Table
-                   | AS TABLE_NAME NT_FIX_MULTIPLE_CONSECUTIVE_AS_PROBLEM
+    if debug:
+        print('DEBUG PARSER:', parsed)
 
-NT_FIX_MULTIPLE_CONSECUTIVE_AS_PROBLEM : empty
-                                       | t_PERIOD TABLE_NAME NT_Auxiliary_Table
-
-NT_WHERE : empty
-         | ORDER_BY T_Condition_ORDER_BY
-         | T_Condition_WHERE NT_Auxiliary_WHERE
-
-NT_Auxiliary_WHERE : empty
-                   | ORDER_BY T_Condition_ORDER_BY
-                   | GROUP_BY T_Condition_GROUP_BY NT_Auxiliary_GROUP_BY
-
-NT_Auxiliary_GROUP_BY : empty
-                      | ORDER_BY T_Condition_ORDER_BY
-                      | Having T_Condition_Having NT_Auxiliary_Having
-
-NT_Auxiliary_Having : empty
-                    | ORDER_BY T_Condition_ORDER_BY
-
-ORDER_BY : ORDER BY
-
-GROUP_BY : GROUP BY
-
-INNER_JOIN : INNER JOIN
-
-LEFT_JOIN : LEFT JOIN
-"""
-# Note 1: NT means "No Terminal", T means "Terminal", empty means "Lambda"
-# Note 2: No need for NT_SELECT and NT_FROM because they must exist no matter what for the SQL query to work
+    return build_select_report(parsed)
